a3cd381f8c4a350a8ff24d2fdd2a5a907b8690e0
danchih <dani.hayashi88@hotmail.com>
2025-09-09 19:23:09 -0300

    configurando web

--- data/frontend/server.js
+++ data/frontend/server.js
@@ -6,8 +6,7 @@ app.use(express.json());
 
 const path = require('path');
 // Servir arquivos estáticos da pasta "public"
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Conexão com MySQL
 const dbConfig = {
@@ -27,22 +26,7 @@ let connection;
     }
 })();
 
-// // Rotas do frontend
-// app.get('/', (req, res) => {
-//     res.send('Frontend rodando e acessando o MySQL!');
-// });
-
 // Rota API que retorna todos os usuários
-app.get('/api/users', async (req, res) => {
-    try {
-        const [rows] = await connection.execute('SELECT * FROM users');
-        res.json(rows);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
-
-// Rota API que retorna todas as receitas
 app.get('/api/receitas', async (req, res) => {
     try {
         const [rows] = await connection.execute('SELECT * FROM receitas');
@@ -51,23 +35,6 @@ app.get('/api/receitas', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-
-
-// // Rota para adicionar usuário
-// app.post('/api/users', async (req, res) => {
-//     try {
-//         const { name, email } = req.body;
-//         const [result] = await connection.execute(
-//             'INSERT INTO users (name, email) VALUES (?, ?)',
-//             [name, email]
-//         );
-//         res.json({ id: result.insertId, name, email });
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
-
-// app.listen(80, () => console.log('Frontend rodando na porta 80'));
 
 const PRIVATE_IP = '192.168.56.12'; // IP da VM frontend na rede privada
 app.listen(80, PRIVATE_IP, () => {
